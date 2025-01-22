8ec528bfc443218a8588b2def33eb21cb760225a
dkskt <dksktm15@gmail.com>
2025-01-22 14:37:51 +0900

    프로젝트 생성

---
+++ src/main/java/com/yong/PlabFootball/PlabFootballApplication.java
@@ -0,0 +1,13 @@
+package com.yong.PlabFootball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlabFootballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlabFootballApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yong/PlabFootball/PlabFootballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yong.PlabFootball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlabFootballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
